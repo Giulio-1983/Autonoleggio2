79e8312c9ea0053bd38f52eed7a49fbb8e197fda
Giulio-1983 <ikneko@hotmail.com>
2024-05-03 13:55:09 +0200

    Non so cosa io abbia modificato ma pusho uguale

--- Autonoleggio/src/Autonoleggio/GestioneAutonoleggio.java
+++ Autonoleggio/src/Autonoleggio/GestioneAutonoleggio.java
@@ -7,6 +7,36 @@ public class GestioneAutonoleggio {
 private List<AutoNoleggiabile> parcoAuto = new ArrayList<>();
 private List<Utente> listaUtenti = new ArrayList<>();
 private List<Batmobile> listaBatmobili = new ArrayList<>();
+
+public GestioneAutonoleggio(List<AutoNoleggiabile> parcoAuto, List<Utente> listaUtenti,
+		List<Batmobile> listaBatmobili) {
+	super();
+	this.parcoAuto = parcoAuto;
+	this.listaUtenti = listaUtenti;
+	this.listaBatmobili = listaBatmobili;
+}
+
+public void caricaFileAuto() {
+	
+}
+
+public void caricaFileUtenti() {
+	
+}
+public void salvaFileAuto() {
+	
+}
+public void salvaFileUtenti() {
+	
+}
+public void caricaFileBatmobili() {
+	
+}
+public void salvaFileBatmobili() {
+	
+}
+
+
 public void cercaAutoCostoDisp() {
 	
 }
