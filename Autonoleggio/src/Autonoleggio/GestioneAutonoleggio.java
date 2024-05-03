aa055bbcb5a565c92c80ac16e855f802d45d8f3f
Giulio-1983 <ikneko@hotmail.com>
2024-05-03 13:09:10 +0200

    Scheletro

---
+++ Autonoleggio/src/Autonoleggio/GestioneAutonoleggio.java
@@ -0,0 +1,47 @@
+package Autonoleggio;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GestioneAutonoleggio {
+private List<AutoNoleggiabile> parcoAuto = new ArrayList<>();
+private List<Utente> listaUtenti = new ArrayList<>();
+private List<Batmobile> listaBatmobili = new ArrayList<>();
+public void cercaAutoCostoDisp() {
+	
+}
+public void mostraAutoDisp() {
+	
+}
+public void cercaAutoMarcaDisp() {
+	
+}
+public void mostraAuto() {
+	
+}
+public void noleggia() {
+	
+}
+public void rimuoviAuto() {
+	
+}
+public void aggiungiAuto() {
+	
+}
+public void cambiaStatoAuto() {
+	
+}
+public void stampaClienti() {
+	
+}
+public void stampaBatmobili() {
+	
+}
+public void aggiungiBatmobile() {
+	
+}
+public void rimuoviBatmobile() {
+	
+}
+
+}
