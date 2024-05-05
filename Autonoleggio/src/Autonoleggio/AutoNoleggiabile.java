d9d1a0ad0fd59adc365d1d93cb19852fb14570f6
Simognafo <167750163+Simognafo@users.noreply.github.com>
2024-05-05 10:31:53 +0200

    Update AutoNoleggiabile.java

--- Autonoleggio/src/Autonoleggio/AutoNoleggiabile.java
+++ Autonoleggio/src/Autonoleggio/AutoNoleggiabile.java
@@ -1,41 +1,48 @@
 package Autonoleggio;
 
 public class AutoNoleggiabile extends Automobile{
-		private boolean isDisponibile;
-	private double costoOrario;
-	private String affidatario;	
+  private boolean isDisponibile;
+  private double costoOrario;
+  private String affidatario;	
 
-	public AutoNoleggiabile(String marca, String modello, String targa, boolean isDisponibile, double costoOrario,
-			String affidatario) {
-		super(marca, modello, targa);
-		this.isDisponibile = isDisponibile;
-		this.costoOrario = costoOrario;
-		this.affidatario = affidatario;
-	}
+  public AutoNoleggiabile(String marca, String modello, String targa, boolean isDisponibile, double costoOrario,
+      String affidatario) {
+    super(marca, modello, targa);
+    this.isDisponibile = isDisponibile;
+    this.costoOrario = costoOrario;
+    this.affidatario = affidatario;
+  }
 
-	public boolean isDisponibile() {
-		return isDisponibile;
-	}
+  public boolean isDisponibile() {
+    return isDisponibile;
+  }
 
-	public void setDisponibile(boolean isDisponibile) {
-		this.isDisponibile = isDisponibile;
-	}
+  public void setDisponibile(boolean isDisponibile) {
+    this.isDisponibile = isDisponibile;
+  }
 
-	public double getCostoOrario() {
-		return costoOrario;
-	}
+  public double getCostoOrario() {
+    return costoOrario;
+  }
 
-	public void setCostoOrario(double costoOrario) {
-		this.costoOrario = costoOrario;
-	}
+  public void setCostoOrario(double costoOrario) {
+    this.costoOrario = costoOrario;
+  }
 
-	public String getAffidatario() {
-		return affidatario;
-	}
+  public String getAffidatario() {
+    return affidatario;
+  }
 
-	public void setAffidatario(String affidatario) {
-		this.affidatario = affidatario;
-	}
+  public void setAffidatario(String affidatario) {
+    this.affidatario = affidatario;
+  }
 
-	
+  public boolean getIsDisponibile(boolean isDisponibile) {
+    return isDisponibile;
+  }
+
+public void setIsDisponibile(boolean isDisponibile) {
+    this.isDisponibile = isDisponibile;  
+}
+
 }
