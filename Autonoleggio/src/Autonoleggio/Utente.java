21ec9755b74657f736ad50b90cb01db6febb24ea
Simognafo <167750163+Simognafo@users.noreply.github.com>
2024-05-05 10:40:40 +0200

    Update Utente.java

--- Autonoleggio/src/Autonoleggio/Utente.java
+++ Autonoleggio/src/Autonoleggio/Utente.java
@@ -1,75 +1,83 @@
 package Autonoleggio;
 
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Utente {
-	private String nome;
-	private String cognome;
-	private String email;
-	private String password;
-	private String ruolo;
-	
-	
-	// Costruttore:
-		Utente(String nome, String cognome , String email, String password, String ruolo) {
-			this.setNome(nome);
-			this.setCognome(cognome);
-			this.setEmail(email);
-			this.setPassword(password);
-			this.setRuolo(ruolo);
+  private String nome;
+  private String cognome;
+  private String email;
+  private String password;
+  private String ruolo;
+
+
+  // Costruttore:
+    Utente(String nome, String cognome , String email, String password, String ruolo) {
+      this.nome = nome;
+      this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+        this.ruolo = "utente";
+      //this.setNome(nome);
+      //this.setCognome(cognome);
+      //this.setEmail(email);
+      //this.setPassword(password);
+      //this.setRuolo("utente");
 }
 
 
-		public String getNome() {
-			return nome;
-		}
+    public String getNome() {
+      return nome;
+    }
 
 
-		public void setNome(String nome) {
-			this.nome = nome;
-		}
+    public void setNome(String nome) {
+      this.nome = nome;
+    }
 
 
-		public String getCognome() {
-			return cognome;
-		}
+    public String getCognome() {
+      return cognome;
+    }
 
 
-		public void setCognome(String cognome) {
-			this.cognome = cognome;
-		}
+    public void setCognome(String cognome) {
+      this.cognome = cognome;
+    }
 
 
-		public String getEmail() {
-			return email;
-		}
+    public String getEmail() {
+      return email;
+    }
 
 
-		public void setEmail(String email) {
-			this.email = email;
-		}
+    public void setEmail(String email) {
+      this.email = email;
+    }
 
 
-		public String getPassword() {
-			return password;
-		}
+    public String getPassword() {
+      return password;
+    }
 
 
-		public void setPassword(String password) {
-			this.password = password;
-		}
+    public void setPassword(String password) {
+      this.password = password;
+    }
 
 
-		public String getRuolo() {
-			return ruolo;
-		}
+    public String getRuolo() {
+      return ruolo;
+    }
 
 
-		public void setRuolo(String ruolo) {
-			this.ruolo = ruolo;
-		}
-		
-	
-		
-		
-		
+    public void setRuolo(String ruolo) {
+      this.ruolo = ruolo;
+    }
+
+  @Override
+    public String toString() {
+      return nome +"," + cognome +"," +  email +"," + password +"," +ruolo;
+    }
+
 }
