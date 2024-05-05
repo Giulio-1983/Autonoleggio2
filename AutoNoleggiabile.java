a673f81fe7ec6d7d9ef9e4f6648229c3ebf80b5d
AnnalisaLupo89 <128742851+AnnalisaLupo89@users.noreply.github.com>
2024-05-05 22:04:01 +0200

    Add files via upload

---
+++ AutoNoleggiabile.java
@@ -0,0 +1,42 @@
+package autonoleggio;
+
+
+public class AutoNoleggiabile extends Automobile{
+	private boolean isDisponibile;
+private double costoOrario;
+private String affidatario;	
+
+public AutoNoleggiabile(String marca, String modello, String targa, boolean isDisponibile, double costoOrario,
+		String affidatario) {
+	super(marca, modello, targa);
+	this.isDisponibile = isDisponibile;
+	this.costoOrario = costoOrario;
+	this.affidatario = affidatario;
+}
+
+public boolean isDisponibile() {
+	return isDisponibile;
+}
+
+public void setDisponibile(boolean isDisponibile) {
+	this.isDisponibile = isDisponibile;
+}
+
+public double getCostoOrario() {
+	return costoOrario;
+}
+
+public void setCostoOrario(double costoOrario) {
+	this.costoOrario = costoOrario;
+}
+
+public String getAffidatario() {
+	return affidatario;
+}
+
+public void setAffidatario(String affidatario) {
+	this.affidatario = affidatario;
+}
+
+
+}
